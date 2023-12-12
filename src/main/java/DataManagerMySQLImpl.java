83a73dcba9e57e9656b616c86d8763e1a0e95939
Tyson Hall <tn8938@gmail.com>
2023-12-12 15:14:57 -0500

    implemented Data Manager MySQLImpl class

--- src/main/java/DataManagerMySQLImpl.java
+++ src/main/java/DataManagerMySQLImpl.java
@@ -1,18 +1,83 @@
-import java.sql.Date;
+import java.sql.*;
+import java.time.LocalDate;
 
 public class DataManagerMySQLImpl implements DataManager {
+    private Connection connection;
+
+    public DataManagerMySQLImpl() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+           connection = DriverManager.getConnection(
+                   "jdbc:mysql://localhost:3306/baibai_petspa", "poweruser", "poweruser");
+        } catch (SQLException e) {
+                throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public Date getPetBirthday(int petId) {
-        return null;
+        Date d = null;
+
+        try (Statement statement = connection.createStatement()) {
+
+            ResultSet birthdayRS = statement.executeQuery("""
+                    SELECT birthday 
+                    FROM pet
+                    WHERE pet_id = 1;
+                    """);
+            birthdayRS.next();
+            d = birthdayRS.getDate("birthday");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return d;
     }
 
     @Override
     public void setPetBirthday(int petId, Date birthday) {
+        try (PreparedStatement ps = connection.prepareStatement("""
+                    UPDATE pet 
+                    SET birthday = ?
+                    WHERE pet_id = ?;
+                    """)) {
 
+            // ps.setDate(1, java.sql.Date.valueOf(LocalDate.now().minusYears(14)));
+            ps.setDate(1, birthday);
+            ps.setInt(2, petId);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public String getPetFullName(int petId) {
-        return null;
+        String fullName = null;
+        String sql = """
+                    SELECT `name`, last_name
+                    FROM pet AS p
+                    JOIN owner AS o
+                    ON p.owner_id = o.owner_id
+                    WHERE p.pet_id = ?;
+                    """;
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setInt(1, petId);
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+            fullName = resultSet.getString("name").concat(" ").concat(resultSet.getString("last_name"));
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return fullName;
+
     }
 }
