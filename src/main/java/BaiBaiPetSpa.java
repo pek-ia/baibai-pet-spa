b240fe869e647e5b90b78b8d14dc93ae507efc70
Tyson Hall <tn8938@gmail.com>
2023-12-12 14:26:10 -0500

    intial commit

--- src/main/java/BaiBaiPetSpa.java
+++ src/main/java/BaiBaiPetSpa.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.sql.*;
 
 public class BaiBaiPetSpa {
     public static void main(String[] args) {
@@ -17,14 +18,34 @@ public class BaiBaiPetSpa {
          *  6. Loop through result set
          */
 
+        Statement statement;
+        ResultSet resultSet;
 
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
+        try (Connection connection =
+                     DriverManager.getConnection(
+                             "jdbc:mysql://localhost:3306/baibai_petspa", "poweruser", "poweruser")) {
+            statement = connection.createStatement();
 
+            String sql = "SELECT * FROM pet";
+            resultSet = statement.executeQuery(sql);
 
+            while (resultSet.next()) {
 
+                String s1 = resultSet.getString("pet_id");
+                String s2 = resultSet.getString("name");
+                String s3 = resultSet.getString("owner_id");
+                System.out.println(s1 + " " + s2 + " " + s3);
+            }
 
-
-
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
         Owner paul = new Owner("Paul", "Kimball", "123 Cherry Lane", "1-800-555-1212");
 
