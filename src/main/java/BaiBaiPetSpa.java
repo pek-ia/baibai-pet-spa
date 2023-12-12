9ff055f398aa778613b7f484eda21e75f03888da
Paul Kimball <pekimball@interfaceassociates.net>
2023-12-12 11:03:07 -0500

    Add list of steps to main

--- src/main/java/BaiBaiPetSpa.java
+++ src/main/java/BaiBaiPetSpa.java
@@ -3,6 +3,29 @@ import java.time.LocalDate;
 public class BaiBaiPetSpa {
     public static void main(String[] args) {
 
+        /*
+         *  1. Add dependencies to pom.xml file for driver
+         *
+         *  2. Load driver class with Class.forName()
+         *
+         *  3. Create Connection with DriverManager
+         *
+         *  4. Create statement
+         *
+         *  5. Execute statement
+         *
+         *  6. Loop through result set
+         */
+
+
+
+
+
+
+
+
+
+
         Owner paul = new Owner("Paul", "Kimball", "123 Cherry Lane", "1-800-555-1212");
 
         Pet baibai = new Pet("Bai-Bai", "Cat", null, paul);
